c6e0c6a5b72d41efdbfc765a2b20c5d544f526b0
Gina Henderson <91082967+ginahend94@users.noreply.github.com>
2022-07-22 22:26:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log('hi')
+const https = (() => {
+var host = "YOURDOMAIN.github.io";
+if ((host == window.location.host) && (window.location.protocol != "https:"))
+    window.location.protocol = "https";
+})();
